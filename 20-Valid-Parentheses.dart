0d104c272ece4e05b4b82fff0949f1471918de30
Mahmoud Elgaml <144808369+MahmoudElgaml@users.noreply.github.com>
2025-02-08 17:16:58 +0200

    [20] [Time Beats: 42.45%] [Memory Beats: 13.11%] - LeetPush

---
+++ 20-Valid-Parentheses.dart
@@ -0,0 +1,23 @@
+class Solution {
+  bool isValid(String s) {
+    final Map<String, String> bracketMap = {
+      ')': '(',
+      '}': '{',
+      ']': '[',
+    };
+    
+    final List<String> stack = [];
+    
+    for (var char in s.split('')) {
+      if (bracketMap.containsValue(char)) {
+        stack.add(char);
+      } else if (bracketMap.containsKey(char)) {
+        if (stack.isEmpty || stack.removeLast() != bracketMap[char]) {
+          return false;
+        }
+      }
+    }
+    
+    return stack.isEmpty;
+  }
+}
